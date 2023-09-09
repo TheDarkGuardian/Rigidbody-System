00dab7d2c8749fda55eda51c145fe286a903025a
TheDarkGuardian <rtrk714@gmail.com>
2023-09-09 11:40:14 +0300

    düzenleme yapıldı

--- Rigidbody-System/Assets/Scripts/RocketController.cs
+++ Rigidbody-System/Assets/Scripts/RocketController.cs
@@ -4,8 +4,6 @@ public class RocketController : MonoBehaviour
 {
     public Vector3 hedefNokta; // Hedef noktanın koordinatları
     public float roketHiz = 10.0f; // Roketin hızı
-    public float yukselmeHizi = 5.0f; // Yükselme hızı
-    public float alcalmaHizi = 2.0f; // Alçalma hızı
 
     private Rigidbody roketRigidbody;
 
@@ -17,27 +15,7 @@ public class RocketController : MonoBehaviour
     void FixedUpdate()
     {
         // Roketin hedefe doğru hareket etmesi
-        Vector3 roketYonu = (hedefNokta - transform.position).normalized;
+        Vector3 roketYonu = (hedefNokta - roketRigidbody.position).normalized;
         roketRigidbody.velocity = roketYonu * roketHiz;
-
-        // Roketin yükseklik değişimini hesapla ve uygula
-        float yukseklikFarki = hedefNokta.y - roketRigidbody.position.y;
-
-/*if (yukseklikFarki > 0)
-{
-    // Yukarı yönde bir kuvvet uygula
-    roketRigidbody.AddForce(Vector3.up * yukselmeHizi);
-}
-else if (yukseklikFarki < 0)
-{
-    // Aşağı yönde bir kuvvet uygula
-    roketRigidbody.AddForce(Vector3.down * alcalmaHizi);
-}*/
-
-        // Yükseklik değişimini uygula (yükselme veya alçalma)
-        /*float yukseklikDegisim = Mathf.Abs(yukseklikFarki);
-        float yukseklikHizi = yukseklikFarki > 0 ? yukselmeHizi : alcalmaHizi;
-        Vector3 yukseklikHareketi = yukseklikYonu * yukseklikHizi * yukseklikDegisim;
-        roketRigidbody.velocity += yukseklikHareketi;*/
     }
 }
